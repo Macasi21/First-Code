738986c93546854bef7abdf562f67a01075e1437
Matheus Caetano <matheuscsilva000@gmail.com>
2025-01-20 22:09:54 -0300

    Hello World

---
+++ main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class main {
+
+    public static int a = 3;
+        public static int b = 5;
+        public static int resultado = a + b;
+    
+        public static void main(String[] args) {
+        
+            System.out.println("Hello World " + resultado);
+    }
+
+}
